bc05c135a3221f95c1a905cb02e3b7fd0b70e4c4
MK4Kard <misha212032@gmail.com>
2025-04-01 21:56:09 +0500

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -1,28 +1,44 @@
-import kotlin.random.Random
-
 fun main() {
-    /*var dayOfWeek: Week = Week.Sunday
-    println("Порядковый номер - ${dayOfWeek.num}")
-    println("${dayOfWeek.Name()} - ${dayOfWeek.Stat()}")
-    print("Погода - ")
-    println(dayOfWeek.Weather())
-    println("Повезёт ли?")
-    println(dayOfWeek.Chance())*/
-    /*println()
-    var paint: Color = Color.YELLOW
-    println("Краска: ${paint.Name()} - цвет")
-    println("склад: ${paint.NumStore()}")
-    print("объем - ")
-    println("${paint.Volume()}")
-    print("устойчивость - ")
-    println("${paint.Stability()}")
-    print("Общая стоимость: ")
-    println("${paint.Sum()}")
-    println()*/
-    var product: Status = Status.Order
-    println("Продукт - ${product.Name_st()}")
-    println(product.Del_time())
-    println(product.Price())
-    product.State_pr()
-    println("Состояние продукта: ${product.state}")
+    try {
+        var dayOfWeek: Week = Week.Sunday
+        println("Порядковый номер - ${dayOfWeek.num}")
+        println("${dayOfWeek.Name()} - ${dayOfWeek.Stat()}")
+        print("Погода - ")
+        println(dayOfWeek.Weather())
+        println("Повезёт ли?")
+        dayOfWeek.Chance()
+        println()
+        var paint: Color = Color.YELLOW
+        println("Краска: ${paint.Name()} - цвет")
+        println("склад: ${paint.NumStore()}")
+        print("объем - ")
+        println("${paint.Volume()}")
+        print("устойчивость - ")
+        println("${paint.Stability()}")
+        println("Количество на складе: ${paint.count_st}")
+        println("Сколько банок возьмёте?")
+        val count = readln()!!.toInt()
+        paint.Count_paint(count)
+        print("Общая стоимость: ")
+        paint.Sum()
+        println()
+        var product: Status = Status.Order
+        println("Продукт - ${product.Name_st()}")
+        println(product.Del_time())
+        product.Price()
+        product.State_pr()
+        println("Состояние продукта: ${product.state}")
+        println("Сколько у вас денег")
+        val pay = readln()!!.toInt()
+        println("Как заплатите: 1 - вся сумма; 2 - долями")
+        val way = readln()!!.toInt()
+        var share = 0
+        if (way == 2) {
+            println("На сколько разделить платёж")
+            share = readln()!!.toInt()
+        }
+        product.Payment(way, pay, share)
+    } catch (e:Exception){
+        println("неверный ввод")
+    }
 }
