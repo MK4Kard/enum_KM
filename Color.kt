c4cf83d1f71df167b0de9ac54bc587b421be828f
MK4Kard <misha212032@gmail.com>
2025-04-01 20:08:19 +0500

    Update Color.kt

--- Color.kt
+++ Color.kt
@@ -1,31 +1,32 @@
 import kotlin.random.Random
 
 enum class Color(var price: Int) {
-    RED(500){
-       override fun Name() = "Красный"
+    RED(50){
+        override fun Name() = "Красный"
         override fun NumStore() = "151"
-       },
-    BLUE(450){
+    },
+    BLUE(45){
         override fun Name() = "Синий"
-    override fun NumStore() = "56Б12"
-             },
-    GREEN(500){
+        override fun NumStore() = "56Б12"
+    },
+    GREEN(50){
         override fun Name() = "Зеленый"
-    override fun NumStore() = "А45"
-              },
-    YELLOW(600){
+        override fun NumStore() = "А45"
+    },
+    YELLOW(60){
         override fun Name() = "Желтый"
-    override fun NumStore() = "А45"
-               },
-    BLACK(520){
+        override fun NumStore() = "А45"
+    },
+    BLACK(52){
         override fun Name() = "Черный"
         override fun NumStore() = "151"
-              },
-    WHITE(525){
+    },
+    WHITE(52){
         override fun Name() = "Белый"
         override fun NumStore() = "151"
     };
 
+    var count = 0
     open fun Name() = "Красный, Синий, Зеленый, Желтый, Черный, Белый"
     open fun NumStore() = "151, А45, 56Б12"
     open var v = 5
@@ -39,7 +40,24 @@ enum class Color(var price: Int) {
         println(rnd)
     }
     open fun Sum(){
-        var sum = price * v
-        println(sum)
+        if (count != 0) {
+            var sum = price * v * count
+            println(sum)
+        }
+    }
+    open fun Count_paint(count: Int){
+        var count_st = Random.nextInt(1, 51)
+        if (count > count_st){
+            println("На складе нет такого количества краски")
+        }
+        else {
+            when {
+                count % 100 in 11..14 -> println("Вы берёте ${count} банок")
+                count % 10 == 1 -> println("Вы берёте ${count} банку")
+                count % 10 in 2..4 -> println("Вы берёте ${count} банки")
+                else -> println("Вы берёте ${count} банок")
+            }
+            this.count = count
+        }
     }
 }
